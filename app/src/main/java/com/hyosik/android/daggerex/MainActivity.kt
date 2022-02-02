e7871190973e27693dd8e677e2eee2dd90d94495
choihyosik <huchoi24@naver.com>
2022-02-03 00:24:36 +0900

    app 에서 테스트 해보기 위해 viewbinding 추가해서 baseActivity 생성

--- app/src/main/java/com/hyosik/android/daggerex/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/daggerex/MainActivity.kt
@@ -2,10 +2,19 @@ package com.hyosik.android.daggerex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyosik.android.daggerex.component.DaggerMyComponent
+import com.hyosik.android.daggerex.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        val myComponent = DaggerMyComponent.create()
+//        binding.titleTextView.text = myComponent.getString()
+
     }
 }
